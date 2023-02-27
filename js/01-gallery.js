c3ea56d82c70f66563d31695b98646df68f5aa9d
Nazar-Kulyk <nazarkulyk1993@gmail.com>
2023-02-28 00:09:48 +0100

    update ok 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryCardsSet = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryCardsSet);
+
+function createGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" 
+            data-source="${original}"
+            alt="${description}">
+          </a>
+        </li>
+      `;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  const clickedElement = event.target;
+  if (clickedElement.nodeName !== "IMG") {
+    return;
+  }
+
+  const originalImageURL = clickedElement.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${originalImageURL}" width="800" height="600">
+  `);
+
+  instance.show();
+}
